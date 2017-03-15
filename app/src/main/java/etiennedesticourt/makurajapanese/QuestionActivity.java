937cbe403c66a43288db775fb3111df614e8d55a
EtienneDesticourt <etienne.desticourt@gmail.com>
2017-03-15 04:00:58 +0100

    Improved answer layout

--- app/src/main/java/etiennedesticourt/makurajapanese/QuestionActivity.java
+++ app/src/main/java/etiennedesticourt/makurajapanese/QuestionActivity.java
@@ -3,9 +3,12 @@ package etiennedesticourt.makurajapanese;
 import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import etiennedesticourt.makurajapanese.Skill.Lesson;
 import etiennedesticourt.makurajapanese.Skill.Question;
@@ -14,10 +17,15 @@ import etiennedesticourt.makurajapanese.databinding.ActivityQuestionBinding;
 
 public class QuestionActivity extends AppCompatActivity {
     public static final String INTENT_LESSON_TAG = "lesson";
+    private final String NEXT_BUTTON_TEXT = "NEXT";
+    private final String VALIDATE_BUTTON_TEXT = "VALIDATE";
+    private final String ANSWER_STATUS_CORRECT = "Correct.";
+    private final String ANSWER_STATUS_INCORRECT = "Incorrect.";
     private Lesson lesson;
     private int currentQuestionId = 0;
     private AnswerFragment answerFragment;
-    ActivityQuestionBinding binding;
+    private ActivityQuestionBinding binding;
+    private boolean validated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,10 +36,12 @@ public class QuestionActivity extends AppCompatActivity {
         lesson = (Lesson) intent.getSerializableExtra(INTENT_LESSON_TAG);
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_question);
+        TextView sentence = (TextView) binding.getRoot().findViewById(R.id.question);
+        sentence.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
         displayQuestion();
     }
 
-    public void nextQuestion(View v) {
+    public void nextQuestion() {
         if (currentQuestionId + 1 >= lesson.getNumQuestions()) {
             //TODO: Switch to next activity;
             return;
@@ -69,6 +79,20 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     public void validateAnswer(View v) {
+        Button button = (Button) v;
+        if (!validated) {
+            displayAnswer();
+            button.setText(NEXT_BUTTON_TEXT);
+            validated = true;
+        }
+        else {
+            nextQuestion();
+            button.setText(VALIDATE_BUTTON_TEXT);
+            validated = false;
+        }
+    }
+
+    private void displayAnswer() {
         String given = answerFragment.getAnswer();
         String actual = lesson.getQuestion(currentQuestionId).getAnswer();
         if (given.equals(actual)) {
@@ -80,12 +104,16 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     private void correctAnswer(){
+        TextView answerStatus = (TextView) findViewById(R.id.answerStatus);
+        answerStatus.setText(ANSWER_STATUS_CORRECT);
         View layout = findViewById(R.id.answerValidationLayout);
         layout.setVisibility(View.VISIBLE);
         layout.setBackgroundResource(R.color.colorQuestionCorrectAnswerBackground);
     }
 
     private void incorrectAnswer() {
+        TextView answerStatus = (TextView) findViewById(R.id.answerStatus);
+        answerStatus.setText(ANSWER_STATUS_INCORRECT);
         View layout = findViewById(R.id.answerValidationLayout);
         layout.setVisibility(View.VISIBLE);
         layout.setBackgroundResource(R.color.colorQuestionIncorrectAnswerBackground);
